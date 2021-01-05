3c382d68784713ff8466ce9a856fc0315688c64a
mayhew64 <mayhew64@yahoo.com>
2021-01-05 17:26:36 -0500

    Added directory recursion and threading

--- find_faces.py
+++ find_faces.py
@@ -6,9 +6,31 @@ import os
 start = time.perf_counter()
 
 KNOWN_FACES_DIR = r'/Volumes/Elements/BrentWood/known_faces'
-UNKNOWN_FACES_DIR = r'/Volumes/Elements/BrentWood/test_pictures/July 7 - General Coverage Part C/Jennifer Glagola-Poomsae - Round 2'
+#UNKNOWN_FACES_DIR = r'/Volumes/Elements/BrentWood/test_pictures/July 7 - General Coverage Part C/Jennifer Glagola-Poomsae - Round 2'
+UNKNOWN_FACES_DIR = r'/Volumes/Elements/BrentWood/test_pictures/July 7 - General Coverage Part C'
 MODEL = 'hog'  # default: 'hog', other one can be 'cnn' - CUDA accelerated (if available) deep-learning pretrained model
 TOLERANCE = 0.60
+
+known_faces = []
+known_names = []
+
+
+
+
+import concurrent.futures
+import math
+import numpy as np
+
+
+URLS = ['http://www.foxnews.com/',
+        'http://www.cnn.com/',
+        'http://europe.wsj.com/',
+        'http://www.bbc.co.uk/',
+        'http://some-made-up-domain.com/']
+
+
+
+
 
 
 def loadKnownFace(faceFileName):
@@ -37,33 +59,7 @@ def findKnownFaces(directory):
             loadKnownFace(f'{directory}/{name}')
 
 
-print('Loading known faces...')
-known_faces = []
-known_names = []
 
-findKnownFaces(KNOWN_FACES_DIR)
-
-# # We oranize known faces as subfolders of KNOWN_FACES_DIR
-# # Each subfolder's name becomes our label (name)
-# for name in os.listdir(KNOWN_FACES_DIR):
-
-#     if (os.path.isdir(name)):
-#         # Next we load every file of faces of known person
-#         for filename in os.listdir(f'{KNOWN_FACES_DIR}/{name}'):
-#             # Load an image
-#             print('Loading ' + filename)
-#             #
-#             try:
-#                 image = face_recognition.load_image_file(f'{KNOWN_FACES_DIR}/{name}/{filename}')
-#                 encoding = face_recognition.face_encodings(image)[0]
-
-#                 # Append encodings and name
-#                 known_faces.append(encoding)
-#                 known_names.append(name)
-#             except IndexError:
-#                 print("No face found in ", filename)
-
-print('Processing unknown faces...')
 
 def loadUnknownFace(faceFileName):
   # Load image
@@ -81,40 +77,64 @@ def loadUnknownFace(faceFileName):
     for face in encodings:
         compared_faces = face_recognition.compare_faces(known_faces,face,TOLERANCE)
         if(True in compared_faces):
-            print(' *** ', known_names[compared_faces.index(True)])
+            print(' *** ', known_names[compared_faces.index(True)], ' found in ',f'{faceFileName}' )
+
+
+# def findUnknownFaces(directory):
+#     print(f'Searching directory: {directory}...',)
+#     # Each subfolder's name becomes our label (name)
+#     for name in os.listdir(directory):
+
+#         if (os.path.isdir(f'{directory}/{name}')):
+#            findUnknownFaces(f'{directory}/{name}')
+#         elif (name.endswith(".png") or name.endswith(".jpg")):   
+#             loadUnknownFace(f'{directory}/{name}')
+
+
+def processUnknowns( files ):
+
+    with concurrent.futures.ThreadPoolExecutor(max_workers=50) as executor:
+        # Start the load operations and mark each future with its URL
+        future_to_file = {executor.submit(loadUnknownFace, file): file for file in files}
+        for future in concurrent.futures.as_completed(future_to_file):
+            file = future_to_file[future]
+            try:
+                data = future.result()
+            except Exception as exc:
+                print('%r generated an exception: %s' % (file, exc))
+            # else:
+            #     print('%r page is %d bytes' % (file, len(data)))
+
 
 
 def findUnknownFaces(directory):
     print(f'Searching directory: {directory}...',)
+    files = []
     # Each subfolder's name becomes our label (name)
     for name in os.listdir(directory):
-
         if (os.path.isdir(f'{directory}/{name}')):
-           findUnknownFaces(f'{directory}/{name}')
-        elif (name.endswith(".png") or name.endswith(".jpg")):   
-            loadUnknownFace(f'{directory}/{name}')
+            findUnknownFaces(f'{directory}/{name}')
+        elif (name.endswith(".png") or name.endswith(".jpg")):
+            files.append(f'{directory}/{name}')
+    
+    num_splits =  math.ceil(len(files)/50)
+    if num_splits > 0 :
+        print('nuber of splits: ', num_splits)
+        split_array = np.array_split(files, num_splits)
 
+        for current_array in split_array:
+            print('split size: ', len(current_array))
+            processUnknowns(current_array)
+
+
+
+
+print('Loading known faces...')
+findKnownFaces(KNOWN_FACES_DIR)
+
+print('Processing unknown faces...')
 findUnknownFaces(UNKNOWN_FACES_DIR)
 
-
-# for filename in os.listdir(UNKNOWN_FACES_DIR):
-
-#     # Load image
-#     print(f'Filename: {filename}...',)
-
-#     image = face_recognition.load_image_file(f'{UNKNOWN_FACES_DIR}/{filename}')
-
-#     # This time we first grab face locations - we'll need them to draw boxes
-#     locations = face_recognition.face_locations(image, model=MODEL)
-
-#     # Now since we know locations, we can pass them to face_encodings as second argument
-#     # Without that it will search for faces once again slowing down whole process
-#     encodings = face_recognition.face_encodings(image, locations)
-#     print(len(encodings),'faces found in', filename)
-#     for face in encodings:
-#         compared_faces = face_recognition.compare_faces(known_faces,face,TOLERANCE)
-#         if(True in compared_faces):
-#             print(known_names[compared_faces.index(True)])
 
 
 
